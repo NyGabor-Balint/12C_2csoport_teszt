f1aab63af6731044e756b334112db416c37c1f15
NyGabor-Balint <nyiszter.gabor.balint@gmail.com>
2024-10-25 13:03:14 +0200

    Körhenger

---
+++ Korhenger/Kor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Korhenger
+{
+    class Kor
+    {
+        private double sugar, terulet, kerulet;
+        public Kor()
+        {
+
+        }
+        public Kor(double r)
+        {
+            sugar = r;
+        }
+        public void SetSugar(double r)
+        {
+            this.sugar = r;
+        }
+        public void SetTerulet()
+        {
+            this.terulet = Math.Round(Math.Pow(this.sugar, 2) * Math.PI);
+        }
+        public void SetKerulet()
+        {
+            this.kerulet = Math.Round(2 * this.sugar * Math.PI);
+        }
+        public double GetTerulet()
+        {
+            return this.terulet;
+        }
+        public double GetKerulet()
+        {
+            return this.kerulet;
+        }
+        public double GetSugar()
+        {
+            return this.sugar;
+        }
+    }
+}
---
+++ Korhenger/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Korhenger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1.példány
+            Kor k1 = new Kor();
+            k1.SetSugar(15);
+            k1.SetKerulet();
+            k1.SetTerulet();
+
+            kiir(k1);
+
+
+            // 2.példány
+
+            Kor k2 = new Kor(58);
+            k2.SetKerulet();
+            k2.SetTerulet();
+
+            kiir(k2);
+            Console.ReadKey();
+        }
+        private static void kiir(Kor k)
+        {
+            Console.WriteLine($"A {k.GetSugar()} sugaru kor kerulete:{k.GetKerulet()},terulete: {k.GetTerulet()}");
+        }
+    }
+    
+}
