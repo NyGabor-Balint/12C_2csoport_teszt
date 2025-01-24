c80142968fae911f3f7cafb0c2cf99a8dcf9e6cd
NyGabor-Balint <nyiszter.gabor.balint@gmail.com>
2025-01-24 12:54:28 +0100

    01.24kesz

--- AtvaltOOP_UnitTeszt/Atvalt_ClassUnitTest.cs
+++ AtvaltOOP_UnitTeszt/Atvalt_ClassUnitTest.cs
@@ -38,7 +38,30 @@ namespace AtvaltOOP_UnitTeszt
 
 
         }
+        [TestMethod]
+        public void isBinaris_True() // A tesztmetodus nevében jelzem, hoyg melyik metodust tesztelem és mire
+        {
+            string szoveg2 = "21";
+            bool vartEredm2 = true, kapottEredm2 = false;
+            // Act- A vizsgált metódus végrehajtása
+            kapottEredm2 = atvalt.isBinaris(szoveg2);
+            // Act- Az eredmény kiértékelése
+            Assert.AreEqual(kapottEredm2, vartEredm2);
 
+
+        }
+        [TestMethod]
+        public void isBinaris_False() // A tesztmetodus nevében jelzem, hoyg melyik metodust tesztelem és mire
+        {
+            string szoveg3 = "k";
+            bool vartEredm3 = true, kapottEredm3 = false;
+            // Act- A vizsgált metódus végrehajtása
+            kapottEredm3 = atvalt.isBinaris(szoveg3);
+            // Act- Az eredmény kiértékelése
+            Assert.AreEqual(kapottEredm3, vartEredm3);
+
+
+        }
 
     }
 }
