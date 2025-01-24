89777e6aad8d516ec18879b407bb3ed8bc580eec
NyGabor-Balint <nyiszter.gabor.balint@gmail.com>
2025-01-24 12:33:10 +0100

    01.24

--- AtvaltOOP_UnitTeszt/Atvalt_ClassUnitTest.cs
+++ AtvaltOOP_UnitTeszt/Atvalt_ClassUnitTest.cs
@@ -13,7 +13,7 @@ namespace AtvaltOOP_UnitTeszt
 
         Atvalt atvalt = new Atvalt();
         [TestMethod]
-        public void TestMethod1() // A tesztmetodus nevében jelzem, hoyg melyik metodust tesztelem és mire
+        public void isDecimal_True() // A tesztmetodus nevében jelzem, hoyg melyik metodust tesztelem és mire
         {
             // Arrange- Tesztkörnyezet beállítása
             string szoveg = "21";
@@ -21,8 +21,21 @@ namespace AtvaltOOP_UnitTeszt
             // Act- A vizsgált metódus végrehajtása
             kapottEredm = atvalt.isDecimal(szoveg);
             // Act- Az eredmény kiértékelése
-            Assert.AreEqual(kapottEredm, vartEredm); 
+            Assert.AreEqual(kapottEredm, vartEredm);
+
+           
+        }
+        public void isBinaris_True() // A tesztmetodus nevében jelzem, hoyg melyik metodust tesztelem és mire
+        {
+            string szoveg1 = "21";
+            bool vartEredm1 = true, kapottEredm1 = false;
+            // Act- A vizsgált metódus végrehajtása
+            kapottEredm1 = atvalt.isBinaris(szoveg1);
+            // Act- Az eredmény kiértékelése
+            Assert.AreEqual(kapottEredm1, vartEredm1);
+
 
         }
+       
     }
 }
